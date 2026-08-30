b1e85b43245a107d99f08bcf3ac656b4fb713286
Nicoli_Hermes <hermesnycoli@gmail.com>
2026-08-30 14:43:30 +0000

    feat: adicionar dados simulados

---
+++ api/src/mocks/user.mock.ts
@@ -0,0 +1,25 @@
+import type { User } from '../types/user.type.ts';
+
+const users: User[] = [{
+    id: 1 ,
+    name : 'alex bessa',
+    email : 'elex@gmail.com',
+    password :'jd*273gsa15'
+    
+} , { 
+    id : 2,
+    name: 'valdiana Bessa',
+    email: 'valdiana@gamil.com',
+    password : 'hdgfhfh1**'
+
+
+} , {   
+    id : 3,
+    name : 'ana bessa',
+    email : 'ana@gamil.com',
+    password : 'pppppp*'
+
+
+}]; 
+
+export default users;
