ccfc74ff85161c5e598ed9e1edcd63dcc131fb78
Nicoli_Hermes <hermesnycoli@gmail.com>
2026-09-06 14:44:45 +0000

    feat: implementar interface de aplicacao para busca de usuario unico

--- api/src/controllers/user.controller.ts
+++ api/src/controllers/user.controller.ts
@@ -1,10 +1,21 @@
 import type { Request , Response} from 'express';
-import { findAllUser } from '../services/user.service.ts';
+import { findAllUsers, findUserById } from '../services/user.service.ts';
 
 
 export function getAllUsers( request : Request , response:Response) {
-    const users = findAllUser();
+    const users = findAllUsers();
 
  response.status(200).json(users);
 
 }
+
+export function getUserById( request : Request, response : Response) {
+    const id = request.params.id;
+
+
+  const user = findUserById(Number(id));
+
+
+  response.status(200).json(user);
+}
+
--- api/src/services/user.service.ts
+++ api/src/services/user.service.ts
@@ -1,7 +1,16 @@
 import users from '../mocks/user.mock.ts';
 
- export function findAllUser() {
+ export function findAllUsers() {
     return users;
 }
 
+export function findUserById(id : number) {
+  const user = users.find(user => user.id === id );
+
+  if (!user) 
+    throw new Error(`Usuario de id ${id} não encontrado.`);
+
+  return user;
+  
+}
 
